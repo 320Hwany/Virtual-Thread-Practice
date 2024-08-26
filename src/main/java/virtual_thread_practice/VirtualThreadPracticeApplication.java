40b4ce6e5e3b7e78537ba67aab7a27081f8130d1
Jung-YouHwan <yhwjd99@gmail.com>
2024-08-26 18:29:15 +0900

    feat : 초기 설정

---
+++ src/main/java/virtual_thread_practice/VirtualThreadPracticeApplication.java
@@ -0,0 +1,13 @@
+package virtual_thread_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualThreadPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/virtual_thread_practice/VirtualThreadPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package virtual_thread_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
