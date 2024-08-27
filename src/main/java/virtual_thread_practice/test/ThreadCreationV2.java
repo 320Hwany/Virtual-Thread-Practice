b8a35a419c85fc0dc15d5a6e6b13d68b6cf80e98
Jung-YouHwan <yhwjd99@gmail.com>
2024-08-27 23:28:58 +0900

    일반 쓰레드 vs 가상 쓰레드 생성 속도 차이 비교

---
+++ src/main/java/virtual_thread_practice/test/ThreadCreationV2.java
@@ -0,0 +1,22 @@
+package virtual_thread_practice.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadCreationV2 {
+
+    public static void main(String[] args) {
+        List<Thread> threads = new ArrayList<>();
+        long startMs = System.currentTimeMillis();
+
+        for (int i = 0; i < 100_000; i++) {
+            Thread thread = Thread.ofVirtual().unstarted(() -> {});
+            threads.add(thread);
+        }
+
+        threads.forEach(Thread::start);
+        long endMs = System.currentTimeMillis();
+
+        System.out.println(endMs - startMs);
+    }
+}
