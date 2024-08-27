fbac1091f54c3421e7863f155ebf5d9edd0b7181
Jung-YouHwan <yhwjd99@gmail.com>
2024-08-27 23:19:57 +0900

    현재 쓰레드 정보 출력

--- src/main/java/virtual_thread_practice/document/VirtualThreadTest.java
+++ src/main/java/virtual_thread_practice/document/VirtualThreadTest.java
@@ -1,4 +1,4 @@
-package virtual_thread_practice.pojo;
+package virtual_thread_practice.document;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -10,15 +10,15 @@ public class VirtualThreadTest {
     public static void main(String[] args) throws Exception {
         // 예제 1
         Thread thread = Thread.ofVirtual()
-                .start(() -> System.out.println("Hello"));
+                .start(() -> System.out.println("Hello : " + Thread.currentThread()));
 
         thread.join();
 
         // 예제 2
         try (ExecutorService myExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
-            Future<?> future = myExecutor.submit(() -> System.out.println("Running thread"));
+            Future<?> future = myExecutor.submit(() -> System.out.println("Running thread : " + Thread.currentThread()));
             future.get();
-            System.out.println("Task completed");
+            System.out.println("Task completed : " + Thread.currentThread());
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
